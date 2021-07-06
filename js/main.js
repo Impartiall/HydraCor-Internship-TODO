2597e2aeca7362ff82571ee79b5e551866e1e29a
Impartiall <jmlooney64@gmail.com>
2021-07-06 14:59:55 -0400

    Make working project

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
-let app = new Vue({
-    el: "#vueApp",
+const app = new Vue({
+    el: '#vue-app',
     data: {
-        welcomeMessage: 'Hello World!'
+        todos: []
     },
     methods: {
-
+        newTodo() {
+            this.todos.push({text: '', completed: false});
+        },
+        toggleCompleted(todo) {
+            const index = this.todos.indexOf(todo);
+            this.todos[index].completed = !this.todos[index].completed;
+        },
+        removeTodo(todo) {
+            this.todos.splice(this.todos.indexOf(todo), 1);
+        }
+    },
+    watch: {
+        todos: {
+            deep: true,
+            handler() {
+                console.log('watch: ' + JSON.stringify(this.todos))
+                localStorage.setItem('todos', JSON.stringify(this.todos));
+            }
+        }
     },
     mounted() {
-
-    },
+        console.log('load: ' + JSON.stringify(this.todos));
+        this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+    }
 });
